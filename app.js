bc15a18a8d0fce7e082778c874bc11cab75fbc7e
IIIAMAH4ik <119852054+IIIAMAH4ik@users.noreply.github.com>
2023-08-19 20:46:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
-let item = "";
+let selectedItems = []; // Здесь будем хранить выбранные продукты
+let buyButton = document.createElement('button');
+buyButton.textContent = 'Купить';
+
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
@@ -15,90 +18,79 @@ let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 let btn7 = document.getElementById("btn7");
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
+btn1.addEventListener('click', function () {
+  selectedItems.push('Экспрессо'); // Добавляем выбранный продукт в массив
+  updateCard(); // Вызываем функцию для обновления отображения корзины
 });
 
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
+btn2.addEventListener('click', function () {
+  selectedItems.push('Капучино');
+  updateCard();
 });
 
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
+btn3.addEventListener('click', function () {
+  selectedItems.push('Американо'); // Добавляем выбранный продукт в массив
+  updateCard(); // Вызываем функцию для обновления отображения корзины
 });
 
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
+btn4.addEventListener('click', function () {
+  selectedItems.push('Мокко');
+  updateCard();
 });
 
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
+btn5.addEventListener('click', function () {
+  selectedItems.push('V-60'); // Добавляем выбранный продукт в массив
+  updateCard(); // Вызываем функцию для обновления отображения корзины
 });
 
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
+btn6.addEventListener('click', function () {
+  selectedItems.push('Латте');
+  updateCard();
 });
 
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 7!");
-		item = "7";
-		tg.MainButton.show();
-	}
+btn7.addEventListener('click', function () {
+  selectedItems.push('Раф'); // Добавляем выбранный продукт в массив
+  updateCard(); // Вызываем функцию для обновления отображения корзины
 });
 
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
 
 
 let usercard = document.getElementById("usercard");
+
+// Создаем функцию для обновления корзины
+function updateCard() {
+  usercard.innerHTML = ''; // Очищаем содержимое корзины
+
+  // Создаем элементы для отображения выбранных продуктов
+  selectedItems.forEach(itemName => {
+    let productItem = document.createElement('div');
+    productItem.textContent = itemName;
+    usercard.appendChild(productItem);
+  });
+
+  // Прикрепляем кнопку "Купить" к корзине
+  usercard.appendChild(buyButton);
+}
+
+// Обработчик клика на кнопке "Купить"
+buyButton.addEventListener('click', function () {
+  if (selectedItems.length === 0) {
+    alert('Добавьте продукты в корзину перед оплатой.');
+  } else {
+    let orderSummary = 'Вы выбрали:\n';
+
+    selectedItems.forEach(itemName => {
+      orderSummary += `- ${itemName}\n`;
+    });
+
+    tg.MainButton.setText('Оплатить'); // Изменяем текст кнопки на "Оплатить"
+    tg.MainButton.show();
+
+    // Отправляем информацию о заказе в Telegram
+    tg.sendData(orderSummary);
+  }
+});
 
 let p = document.createElement("p");
 
